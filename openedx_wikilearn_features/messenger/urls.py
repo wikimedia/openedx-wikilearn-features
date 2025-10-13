c2f3f567efd29a78689cb816d47e35be7a39f420
eemaanamir <eemaan.amir@gmail.com>
2025-10-13 13:13:26 +0500

    feat: added messenger app

--- openedx_wikilearn_features/messenger/urls.py
+++ openedx_wikilearn_features/messenger/urls.py
@@ -4,16 +4,10 @@ Urls for Messenger
 from django.conf.urls import include
 from django.urls import re_path
 
-from  openedx_wikilearn_features.messenger.views import render_messenger_home
 
 app_name = 'messenger'
 
 urlpatterns = [
-    re_path(
-        r'^$',
-        render_messenger_home,
-        name='messenger_home'
-    ),
     re_path(
         r'^api/v0/',
         include('openedx_wikilearn_features.messenger.api.v0.urls', namespace='messenger_api_v0')
