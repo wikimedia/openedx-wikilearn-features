cc4b1dfd0329ec9a9da1bee96833c5626d19a49a
eemaanamir <eemaan.amir@gmail.com>
2025-11-25 02:04:16 +0500

    feat: added urls and styling for reports in instructor tab

--- openedx_wikilearn_features/wikimedia_general/urls.py
+++ openedx_wikilearn_features/wikimedia_general/urls.py
@@ -3,7 +3,9 @@ URLs for the general features
 """
 
 from django.urls import include, path
-
+from openedx_wikilearn_features.wikimedia_general.djangoapps_patches.instructor.views.utils import (
+    list_report_downloads_student_admin,
+)
 app_name = "wikimedia_general"
 
 urlpatterns = [
@@ -16,4 +18,6 @@ urlpatterns = [
             )
         ),
     ),
+    path('<course_id>/list_report_downloads_student_admin/',
+         list_report_downloads_student_admin, name='list_report_downloads_student_admin'),
 ]
