0763920caf421aefeb3cdc80dbe2c2005de70c82
Ahmed Khalid <ahmed.khalid@arbisoft.com>
2025-11-27 23:02:54 +0500

    feat: add course pace and topic filters to catalog

--- openedx_wikilearn_features/wikimedia_general/apps.py
+++ openedx_wikilearn_features/wikimedia_general/apps.py
@@ -34,7 +34,9 @@ class WikimediaGeneralConfig(AppConfig):
     }
 
     def ready(self):
-        import openedx_wikilearn_features.wikimedia_general.signals  # pylint: disable=unused-import
-        from openedx_wikilearn_features.wikimedia_general.utils import load_core_patches  # pylint: disable=import-outside-toplevel
+        import openedx_wikilearn_features.wikimedia_general.signals  # pylint: disable=unused-import  # noqa: F401
+        from openedx_wikilearn_features.wikimedia_general.utils import (
+            load_core_patches,  # pylint: disable=import-outside-toplevel
+        )
 
         load_core_patches()
