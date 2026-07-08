0f12a930fe8bae0d5b93193b87beb572bead9583
Ahmed Khalid <106074266+ahmed-arb@users.noreply.github.com>
2026-07-08 11:25:53 +0500

    fix: build DiscoverCourses bundle from the plugin and port router to v6 (#35)

--- openedx_wikilearn_features/meta_translations/static/discover_courses/js/Navigator.js
+++ openedx_wikilearn_features/meta_translations/static/discover_courses/js/Navigator.js
@@ -1,19 +1,15 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CourseTranslations from "./components/CourseTranslations";
 import DiscoverCoursesContent from "./components/DiscoverCoursesContent";
 
 export default function Navigator({ context }) {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/meta_translations/discover_courses/">
-          <DiscoverCoursesContent context={context} />
-        </Route>
-        <Route exact path="/meta_translations/discover_courses/:course_id">
-          <CourseTranslations context={context} />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/meta_translations/discover_courses/" element={<DiscoverCoursesContent context={context} />} />
+        <Route path="/meta_translations/discover_courses/:course_id" element={<CourseTranslations context={context} />} />
+      </Routes>
     </BrowserRouter>
   );
 }
