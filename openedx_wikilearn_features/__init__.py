cb33df6014ae23c38cc2c5b522c319a46f77f911
eemaanamir <eemaan.amir@gmail.com>
2026-09-21 13:43:04 +0500

    chore: bump version to 1.0.5

--- openedx_wikilearn_features/__init__.py
+++ openedx_wikilearn_features/__init__.py
@@ -5,6 +5,6 @@ Wikilearn features for Open edX
 import os
 from pathlib import Path
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
 ROOT_DIRECTORY = Path(os.path.dirname(os.path.abspath(__file__)))
